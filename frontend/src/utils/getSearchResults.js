a070760e4f857b17cafcbf366f226be363ac47aa
Martin Holt Juliussen <martinhj@gmail.com>
2024-01-23 12:46:39 +0100

    revert fetch w/ native

--- frontend/src/utils/getSearchResults.js
+++ frontend/src/utils/getSearchResults.js
@@ -1,28 +1,23 @@
-// import groq from 'groq';
-// import { clientGROQ } from '../../groq/client';
+import groq from 'groq';
+import { clientGROQ } from '../../groq/client';
 
-// const getSearchResults = async ({ language }) =>
-const getSearchResults = async () => {
-  // const groqQuery = groq`
-  //   {
-  //     "all": *[
-  //       _type in ["Activity", "Article", "Page", "Place", "Region", "Service", "Provider", "ThingsToDoPage", "DayTrip"]
-  //     ] | [0...10] { _id }
-  //   }
-  // `;
+const getSearchResults = async ({ language }) => {
+  const groqQuery = groq`
+    {
+      "all": *[
+        _type in ["Activity", "Article", "Page", "Place", "Region", "Service", "Provider", "ThingsToDoPage", "DayTrip"]
+      ] | [0...10] { _id }
+    }
+  `;
 
-  // const results = await clientGROQ.fetch(groqQuery, {
-  //   query: '',
-  //   categoryId: '',
-  //   language,
-  //   fallback: 'en',
-  // });
+  const results = await clientGROQ.fetch(groqQuery, {
+    query: '',
+    categoryId: '',
+    language,
+    fallback: 'en',
+  });
 
-  const results = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json));
-
-  return { results: { all: [results] } };
+  return { results };
 };
 
 export default getSearchResults;
